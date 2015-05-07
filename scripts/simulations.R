bf98752d4cbc009204f67db18da5eed702a799c5
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-05-06 21:42:06 -0700

    add some changes towards geno specific canopy model

--- scripts/simulations.R
+++ scripts/simulations.R
@@ -3,30 +3,32 @@ library(YplantQMC)
 installQuasiMC
 
 ls()
-
+setwd("/Users/Cody_2/git.repos/3D_reconstruction/data/Yplant")
 sugarmaple <- constructplant("sugarmaple.p", "leaffile.l")
 plot(sugarmaple)
 summary(sugarmaple)
 str(sugarmaple)
 class(sugarmaple)
-sydney <- setLocation(lat=-33.8, long=151.2)
-plot(sydney)
-sunnyday <- setMet(sydney, month=12, day=12, nsteps=12, Tmin=12, Tmax=28, PARday=24)
 
+?setLocation
+laramie <- setLocation(lat = 41.3167, long = -105.5833)
+plot(laramie)
+?setMet
+sunnyday <- setMet(laramie, month = 7, day = 12, nsteps = 12, Tmin = 12, Tmax = 28, PARday = 24)
 plot(sunnyday)
-setPhy
 
-clidlrc <- setPhy("lightresponse",
-leafpars=list(Amax=8.5, Rd=0.7, phi=0.045, theta=0.95, reflec=0.1, transmit=0.05))
+?setPhy
+clidlrc <- setPhy("lightresponse",leafpars=list(Amax=8.5, Rd=0.7, phi=0.045, theta=0.95, reflec=0.1, transmit=0.05))
 YplantDay
 
-maplerun <- YplantDay(sugarmaple, met=sunnyday, phy=clidlrc, hemi=largegap)
+?YplantDay
+maplerun <- YplantDay(sugarmaple, met = sunnyday, phy=clidlrc, hemi=largegap)
 head(maplerun)
 str(maplerun)
 ?randomplant
+summary(sugarmaple)
 
-coneplant <- constructplant(randomplant(radius=250, height=500, 
-leaflen=25, LA=1))
+coneplant <- constructplant(randomplant(radius=250, height=500, leaflen=25, LA=1))
 
 plot.leaffile(sugarmaple.l)
 plot(readl("LeafFile.l"))
