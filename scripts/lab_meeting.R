fb6ad570a95357242a7ea13b685c0d917b36c596
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-08-23 23:50:11 -0700

    rotations of 2D coordinates

--- scripts/lab_meeting.R
+++ scripts/lab_meeting.R
@@ -21,3 +21,68 @@ brassicastand <- makeStand(list(brassica,brassica,brassica,brassica,brassica),
 
 plot(brassicastand)
 
+leaffile <- as.data.frame(read.table("brassica_leaf.txt", sep = "\t", header = TRUE))
+test <- leaffile
+test <- test*25.4
+test$X <- test$X - test[1,1]
+test$Y <- test$Y - test[1,2]
+test 
+test <- as.data.frame(t(test))
+plot(test)
+test
+C <- matrix(c(cos(0.6722195),asin(0.6722195),sin(0.6722195),cos(0.6722195)), nrow = 2, ncol = 2)
+C
+
+B%*%test[,1]
+B%*%test[,2]
+
+newangle <- atan2(91.1098, 114.4778 )
+
+plot(c(1, 6), c(2, 2), type="n", xlim=c(0, 7), ylim=c(-2, 6))
+segments(1, 1, 6, 1)
+segments(1, 3, 6, 3)
+
+
+
+# coordinate transform: cartesian plane rotation
+xyrot<-function(pairs,ang){
+    # pairs must be Nx2 matrix w/ x in first column and y in second
+    xrot <- pairs[,1]*cos(ang) - pairs[,2]*sin(ang)
+    yrot <- pairs[,1]*sin(ang) + pairs[,2]*cos(ang)
+    return(invisible(cbind(xrot,yrot)))
+}
+
+test2 <- as.matrix(test)
+ang <- -(pi/2)
+out <- xyrot(test2, ang)
+out
+plot(test2)
+plot(out)
+out <- xyrot(out, newangle)
+out
+plot(out)
+
+out2 <- acos((114.4778-0)/sqrt((91.1098-0)^2 + (114.4778)^2))
+out2
+
+C <- matrix(c(cos(0.6722195),asin(0.6722195),sin(0.6722195),cos(0.6722195)), nrow = 2, ncol = 2)
+C
+
+out3 <- as.data.frame(t(out))
+plot(out)
+
+
+out3[,1] <- C%*%out3[,1]
+out3[,2] <- C%*%out3[,2]
+out3[,3] <- C%*%out3[,3]
+out3[,4] <- C%*%out3[,4]
+out3[,5] <- C%*%out3[,5]
+out3[,6] <- C%*%out3[,6]
+out3
+out4 <- as.data.frame(t(out3))
+out4
+plot(out4)
+
+out4 <- apply(C,out3,prod)
+
+
