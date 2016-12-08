bcce1457cac9a14fc15c1cf17b61c697c3953bf2
Cody Markelz <rjmarkelz@ucdavis.edu>
2016-12-08 11:58:24 -0800

    brassica shelf

--- scripts/brassica_canopy.R
+++ scripts/brassica_canopy.R
@@ -17,9 +17,8 @@ plot(laramie)
 
 sunnyday <- setMet(laramie, month = 7, day = 12, nsteps = 12, Tmin = 12, Tmax = 28, PARday = 24)
 plot(sunnyday)
-
-test2$phy <- setPhy("lightresponse",leafpars=list(Amax=25, Rd=0.7, phi=0.045, theta=0.95, reflec=0.1, transmit=0.05))
-testrun <- YplantDay(test2, met = sunnyday, phy = test2$phy)
+brassica$phy <- setPhy("lightresponse",leafpars=list(Amax=25, Rd=0.7, phi=0.045, theta=0.95, reflec=0.1, transmit=0.05))
+testrun <- YplantDay(brassica, met = sunnyday, phy = brassica$phy)
 
 psrdata(testrun)
 plot(testrun)
@@ -28,3 +27,7 @@ br_stand <- makeStand(list(brassica, brassica, brassica, brassica, brassica),
                        xyz=data.frame(x=c(0,20,5),
                                       y=c(0,0,5),
                                       z=c(0,0,0)))
+
+?lightresponse
+out <- lightresponse(PAR = (100:1000), Amax= 100, phi = 0.05, theta = 0.5, Rd = 1)
+plot(out)
